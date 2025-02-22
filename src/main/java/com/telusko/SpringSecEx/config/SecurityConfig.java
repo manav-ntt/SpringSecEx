3b0a85615d457852c6e00a6069e873c500daf6b2
Manav-NTT <manav12.gupta@nttdata.com>
2025-02-23 03:01:59 +0530

    added hardcoded JWT Token

--- src/main/java/com/telusko/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/telusko/SpringSecEx/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.telusko.SpringSecEx.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,8 +26,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(customizer -> customizer.disable());
-        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("register", "login").permitAll() .anyRequest().authenticated());
+//        httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
@@ -38,6 +40,11 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     /*@Bean
--- src/main/java/com/telusko/SpringSecEx/service/UserService.java
+++ src/main/java/com/telusko/SpringSecEx/service/UserService.java
@@ -3,6 +3,9 @@ package com.telusko.SpringSecEx.service;
 import com.telusko.SpringSecEx.model.Users;
 import com.telusko.SpringSecEx.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,12 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    private JWTService jwtService;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user) {
@@ -19,4 +28,12 @@ public class UserService {
         return repo.save(user);
     }
 
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken();
+        } else {
+            return "fail";
+        }
+    }
 }
